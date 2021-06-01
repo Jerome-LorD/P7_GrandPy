90c06da5c60f034b35be1aa2a06daff7e683eac6
Jerome-LorD <jerome-divry@laposte.net>
2021-06-01 17:45:28 +0200

    Add utils.py

---
+++ app/models/utils.py
@@ -0,0 +1,12 @@
+hello = {
+    "Bonjour": "Bonjour cher visiteur, voici : ",
+    "Bonjour": "Bonjour mon pousin, voici : ",
+    "Bonjour": "Bonjour mon canard, voici : ",
+    "Salut": "Salut toi, j'ai l'adresse que tu cherches : ",
+    "Hello": "Hiii ! How are you ? J'ai trouvé ton bonheur : ",
+}
+
+moreInfo = [
+    "Mais t'ai-je déjà raconté ",
+    "l'histoire de ce quartier qui m'a vu en culottes courtes ? ",
+]
