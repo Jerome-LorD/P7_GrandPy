e9ce02c3ac09b65223d0768c12dc5b1b81ce7b4d
Jerome-LorD <jerome-divry@laposte.net>
2021-06-01 17:50:57 +0200

    update config

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
 import os
 
+from pathlib import Path
+from dotenv import load_dotenv, find_dotenv  # type: ignore
 
-class Config(object):
+load_dotenv(find_dotenv())
+
+
+class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "no way"
+    GOOGLE_KEY = os.getenv("APIKEY")
+    GOOGLE_SIGN = os.getenv("SIGNATURE")
+
+    BASE_DIR = Path(".")
+    APP_DIR = BASE_DIR / "app"
+    JSON_DIR = BASE_DIR / APP_DIR / "json"
