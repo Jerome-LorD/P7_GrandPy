f8bada51b8ccf12bc490873c18c53746da13f704
Jerome-LorD <jerome-divry@laposte.net>
2021-06-08 15:23:24 +0200

    rename method replace_malicious_text & refactoring

--- app/models/parser.py
+++ app/models/parser.py
@@ -12,40 +12,42 @@ class Parser:
         """Init."""
 
         self.selected_words = []
-        self.quotePattern = r"l'|d'|m'|n'|qu'|s'|t'|\,|\?|\.|\!"
+        self.quote_pattern = r"l'|d'|m'|n'|qu'|s'|t'|\,|\?|\.|\!"
 
     def stop_words(self):
         """Load a french word list from file."""
         with open(Config.JSON_DIR / "fr.json", encoding="utf-8") as f:
             return json.load(f)
 
-    def remove_defined_articles(self, text):
+    def remove_defined_articles(self, text: str) -> str:
         """Remove french defined articles with apostrophe."""
-        cleaned_text = re.sub(self.quotePattern, "", text)
+        cleaned_text = re.sub(self.quote_pattern, "", text)
         return cleaned_text.strip()
 
-    def replace_malicious_text(self, text):
-        """Relace malicious text."""
-        refusedChars = r"[\<\>]{1,}|[$|*|£|¤|#|~|&|`|^|\"]{2,}|(.)\1{4,}|\
-        \W*(alert)\W*|\W*(script)\W*"
-        goodText = "montmartre"
-        return goodText if re.search(refusedChars, text) else text
+    def replace_unwanted_chars(self, text: str) -> str:
+        """Replace unwanted chars."""
+        not_chars = r"[\<\>]{1,}|[$|*|£|¤|#|~|&|`|^|\"]{1,}|(.)\1{4,}|\d{1,}|\W*(alert)\W*|\W*(script)\W*"
+        replacement_text = "montmartre"
+        return replacement_text if re.search(not_chars, text) else text
 
-    def sanitize_text(self, text):
+    def sanitize_text(self, text: str) -> str:
         """Remove str in stop_word list."""
-        lowerTxt = text.lower()
+        low_txt = text.lower()
         return " ".join(
-            [word for word in lowerTxt.split() if word not in self.stop_words()]
+            [word for word in low_txt.split() if word not in self.stop_words()]
         )
 
-    def isolate_target(self, text):
+    def isolate_target(self, text: str) -> str:
+        """Isolate targeted words."""
         for word in text.split():
             if word in hello.keys():
                 target = text.replace(word, "")
+                print(target.strip())
                 return target.strip()
+        print(text)
         return text
 
-    def greetings(self, text):
+    def greetings(self, text: str) -> str:
         """Return the same greetings as those sent."""
         greets = [hello[word] for word in text.split() if word in hello.keys()]
         return "".join(greets) if len(greets) > 0 else "Des infos :"
