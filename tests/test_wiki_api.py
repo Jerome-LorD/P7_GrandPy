35d92ef1bdd782eae9d86b769957de40eb6c8a62
Jerome-LorD <jerome-divry@laposte.net>
2021-06-14 07:05:46 +0200

    refactoring

--- tests/test_wiki_api.py
+++ tests/test_wiki_api.py
@@ -1,17 +1,56 @@
 #!/usr/bin/env python
 """Tests wiki API."""
+import requests
+
+from app.models.wiki_api import WikiAPI
+from config import Config
+
+wiki = WikiAPI()
 
 
-class MockResponse:
+class WikiFakeResponse:
+    """Wiki API fake response of requests get."""
+
     def json(self):
-        return {"mock_key": "mock_response"}
+        """Fake json."""
+        return {"query": {"pages": {"12345": {"extract": "mock_response"}}}}
 
 
-def mock_extract_data(coords):
-    return {"mock_key": "mock_response"}
+class WikiWrongFakeResponse:
+    """Wiki API wrong fake response class."""
+
+    def json(self):
+        """Fake json."""
+        return {"mock_key": ""}
 
 
-def test_mock_extract_data(monkeypatch):
-    monkeypatch.setattr("app.models.wiki_api.WikiAPI.extract_data", mock_extract_data)
-    result = mock_extract_data("73.45|2.65")
-    assert result == {"mock_key": "mock_response"}
+def test_extract_data(monkeypatch):
+    """Test extract_data monkeypatched."""
+
+    def mock_requests_get(*args, **kwargs):
+        """Mock request get returns Wiki fake response."""
+        return WikiFakeResponse()
+
+    monkeypatch.setattr(requests, "get", mock_requests_get)
+
+    result = wiki.extract_data({"location": {"lat": 43.07945, "lng": -79.07472}})
+    assert result == "mock_response"
+
+
+def test_extract_data_wrong_key(monkeypatch):
+    """Test the return from a wrong key."""
+
+    def mock_requests_get(*args, **kwargs):
+        """Mock request get returns Wiki wrong fake response."""
+        return WikiWrongFakeResponse()
+
+    monkeypatch.setattr(requests, "get", mock_requests_get)
+
+    result = wiki.extract_data("test")
+    assert result == Config.MSG_NO_TEXT_FOUND
+
+
+def test_integration_extract_data():
+    """Integration test of extract data."""
+    result = wiki.extract_data({"location": {"lat": 43.07945, "lng": -79.07472}})
+    assert result.startswith("Les chutes du Niagara")
