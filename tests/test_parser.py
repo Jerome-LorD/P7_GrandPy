3f35793bbcfb9233f6ddc4bb717bc3bb9a8dad9f
Jerome-LorD <jerome-divry@laposte.net>
2021-06-14 07:06:44 +0200

    refactoring

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,3 +1,4 @@
+"""Parser tests module."""
 import pytest
 
 from app.models.parser import Parser
@@ -6,6 +7,7 @@ from app.models.parser import Parser
 quest1 = "Salut GrandPy, quelle est l'adresse de la Tour Eiffel s'il te plait ?"
 quest2 = "Hello, <script>alert('bla')</script>"
 quest3 = "Hello GrandPy, montre moi les Chutes du Niagara"
+quest4 = "Affiche la tour eiffel le con."
 
 
 @pytest.mark.parametrize(
@@ -17,10 +19,9 @@ quest3 = "Hello GrandPy, montre moi les Chutes du Niagara"
     ],
 )
 def test_remove_defined_articles(base_text, cleaned_text):
+    """Test returned words no longer have French defined articles."""
     parser = Parser()
-
     goodText = parser.replace_unwanted_chars(base_text)
-    print("mon test", base_text, goodText)
     result = parser.remove_defined_articles(goodText)
     assert result == cleaned_text
 
@@ -33,7 +34,8 @@ def test_remove_defined_articles(base_text, cleaned_text):
         (quest3, "hello chutes niagara"),
     ],
 )
-def test_sanitize_text(base_text, cleaned_text):
+def test_if_stopWords_items_deleted_keeping_important_words(base_text, cleaned_text):
+    """Test if the stopWords elements are deleted keeping the important words."""
     parser = Parser()
     question = parser.replace_unwanted_chars(base_text)
     isolated_kw = parser.remove_defined_articles(question)
@@ -45,7 +47,8 @@ def test_sanitize_text(base_text, cleaned_text):
     "base_text, cleaned_text",
     [(quest1, "tour eiffel"), (quest2, "montmartre"), (quest3, "chutes niagara")],
 )
-def test_isolate_target(base_text, cleaned_text):
+def test_if_targeted_words_match_those_expected(base_text, cleaned_text):
+    """Test if targeted words match those expected."""
     parser = Parser()
     question = parser.replace_unwanted_chars(base_text)
     isolated_kw = parser.remove_defined_articles(question)
@@ -57,15 +60,29 @@ def test_isolate_target(base_text, cleaned_text):
 @pytest.mark.parametrize(
     "base_text, cleaned_text",
     [
-        (quest1, "Salut toi, j'ai l'adresse que tu cherches : "),
-        (quest2, "Des infos :"),
-        (quest3, "Hiii ! How are you ? J'ai trouvé ton bonheur : "),
+        (quest1, "Salut toi, bien sûr, voici tes infos : "),
+        (quest2, "Des infos : "),
+        (quest3, "Hiii ! How are you ? Voici tes infos : "),
     ],
 )
-def test_greetings(base_text, cleaned_text):
+def test_if_the_greeting_returned_the_same_as_the_one_given(base_text, cleaned_text):
+    """Use the same greetings in return."""
     parser = Parser()
     question = parser.replace_unwanted_chars(base_text)
     isolated_kw = parser.remove_defined_articles(question)
     sanitized_txt = parser.sanitize_text(isolated_kw)
     result = parser.greetings(sanitized_txt)
+    assert result == cleaned_text
+
+
+@pytest.mark.parametrize(
+    "base_text, cleaned_text",
+    [
+        (quest4, "Affiche la tour eiffel le ****."),
+    ],
+)
+def test_if_the_insults_are_replaced_by_stars(base_text, cleaned_text):
+    """Verify if inslts are replaced by stars in text."""
+    parser = Parser()
+    result = parser.replace_insult_to_stars(base_text)
     assert result == cleaned_text
