6693597276c5e20f84a609da22e7dd282b62545c
Jerome-LorD <jerome-divry@laposte.net>
2021-06-14 06:59:48 +0200

    refactoring

--- app/models/google_api.py
+++ app/models/google_api.py
@@ -6,16 +6,13 @@ from config import Config
 
 
 class GoogleAPI:
-    """Tests wiki."""
+    """Class Google API."""
 
     def __init__(self):
         """Init."""
-
         self.url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
 
-        self.payload = {
-            "key": Config.GOOGLE_KEY,
-        }
+        self.payload = {"key": Config.PLACES_KEY, "language": "fr"}
 
         self.headers = {
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
@@ -27,12 +24,18 @@ class GoogleAPI:
             self.payload["query"] = keywords
             req = requests.get(self.url, headers=self.headers, params=self.payload)
             data = req.json()
+            if data["status"] == "OK":
 
-            return {
-                "location": data["results"][0]["geometry"]["location"],
-                "address": data["results"][0]["formatted_address"],
-                "name": data["results"][0]["name"],
-            }
+                return {
+                    "location": data["results"][0]["geometry"]["location"],
+                    "address": data["results"][0]["formatted_address"],
+                    "name": data["results"][0]["name"],
+                }
+            else:
+                return "Je n'ai pas compris."
 
         except requests.exceptions.RequestException as e:
-            raise SystemExit(e)
+            raise e
+
+        except KeyError:
+            return "Je n'ai pas compris."
