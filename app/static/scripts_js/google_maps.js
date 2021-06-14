6b6da3afa44675e41eb43f5443974a9222c7aed7
Jerome-LorD <jerome-divry@laposte.net>
2021-06-14 09:18:49 +0200

    some modifs

--- app/static/scripts_js/google_maps.js
+++ app/static/scripts_js/google_maps.js
@@ -1,11 +1,16 @@
 let map;
+let map2;
+let p = document.createElement("p");
+p.id = "map";
+
 function initMap(location, name) {
     if (location !== undefined) {
-
-        map = new google.maps.Map(document.getElementById("map"), {
+        document.querySelector("#resp").appendChild(p);
+        let payload = {
             center: location,
             zoom: 15,
-        });
+        }
+        map = new google.maps.Map(document.getElementById("map"), payload);
         new google.maps.Marker({
             position: location,
             map,
