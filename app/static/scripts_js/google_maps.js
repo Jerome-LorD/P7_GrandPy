356a995511cf8906ddcb05ef5b299414911bfc8a
Jerome-LorD <jerome-divry@laposte.net>
2021-06-14 19:33:58 +0200

    refactoring

--- app/static/scripts_js/google_maps.js
+++ app/static/scripts_js/google_maps.js
@@ -1,16 +1,12 @@
 let map;
-let map2;
-let p = document.createElement("p");
-p.id = "map";
 
 function initMap(location, name) {
     if (location !== undefined) {
-        document.querySelector("#resp").appendChild(p);
         let payload = {
             center: location,
             zoom: 15,
         }
-        map = new google.maps.Map(document.getElementById("map"), payload);
+        map = new google.maps.Map(document.querySelector(".map:last-child"), payload);
         new google.maps.Marker({
             position: location,
             map,
