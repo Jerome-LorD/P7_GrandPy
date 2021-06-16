a199f8e59321f78dc4de62465925becb7802b649
Jerome-LorD <jerome-divry@laposte.net>
2021-06-16 19:45:50 +0200

    some adds

--- app/models/utils.py
+++ app/models/utils.py
@@ -4,11 +4,14 @@ hello = {
     "salut": "Salut toi, bien sûr, voici tes infos : ",
     "hello": "Hiii ! How are you ? Voici tes infos : ",
     "coucou": "Coucou ! Voici tes infos : ",
+    "wesh": "Wesh ! Voici tes infos : ",
+    "hey": "Hey you ! Voici tes infos : ",
 }
 
+# simple swearword sample example... but there're many others.
 insult_pattern = r"connard|conne|\bcon\b|pute|salope|connasse|bite|couille|débile"
 
 moreInfo = [
-    "Et je peux te dire que " "Mais t'ai-je déjà raconté ",
-    "l'histoire de ce quartier qui m'a vu en culottes courtes ? ",
+    "Mais t'ai-je déjà raconté l'histoire"
+    "de ce quartier qui m'a vu en culottes courtes ? "
 ]
