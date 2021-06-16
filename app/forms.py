4b3171402dec54eab8a9992c2513bfd57aeff429
Jerome-LorD <jerome-divry@laposte.net>
2021-06-16 19:43:51 +0200

    rm forms.py

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-
-
-class ResearchForm(FlaskForm):
-    txt_area = TextAreaField(
-        "TextArea", render_kw={"placeholder": "posez-moi votre question"}
-    )
-    submit = SubmitField("Rechercher")
