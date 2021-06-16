fe08473441718b2cf2cefe0ecd88d62a30b3de15
Jerome-LorD <jerome-divry@laposte.net>
2021-06-16 19:44:49 +0200

    some refactoring

--- app/models/google_api.py
+++ app/models/google_api.py
@@ -12,7 +12,7 @@ class GoogleAPI:
         """Init."""
         self.url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
 
-        self.payload = {"key": Config.PLACES_KEY, "language": "fr"}
+        self.payload = {"key": Config.PLACES_API_KEY, "language": "fr"}
 
         self.headers = {
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
@@ -25,14 +25,11 @@ class GoogleAPI:
             req = requests.get(self.url, headers=self.headers, params=self.payload)
             data = req.json()
             if data["status"] == "OK":
-
                 return {
                     "location": data["results"][0]["geometry"]["location"],
                     "address": data["results"][0]["formatted_address"],
                     "name": data["results"][0]["name"],
                 }
-            else:
-                return "Je n'ai pas compris."
 
         except requests.exceptions.RequestException as e:
             raise e
